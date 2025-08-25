2e698392cd109c9da2aa46b2f1bd1f4efb2fb675
Mimonata <sabina_pitul@yahoo.de>
2025-08-25 11:30:55 +0200

    ex01

---
+++ ex01/Span.cpp
@@ -0,0 +1,77 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Span.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: spitul <spitul@student.42berlin.de>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/24 12:11:23 by spitul            #+#    #+#             */
+/*   Updated: 2025/08/25 11:30:20 by spitul           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Span.hpp"
+#include <stdexcept>
+#include <algorithm>
+#include <climits>
+
+Span::Span(unsigned int N)	: _N(N)
+{
+	
+}
+
+Span::Span(const Span &other)
+{
+	this->_v = other._v;
+	this->_N = other._N;
+}
+
+Span& Span::operator=(const Span &other)
+{
+	if (this != &other)
+	{
+		_v = other._v;
+		_N = other._N;
+	}
+	return *this;
+}
+
+Span::~Span()
+{
+	
+}
+
+void	Span::addNumber(int nb)
+{
+	if (_v.size() >= _N)
+		throw std::length_error("Span vector already full");
+	_v.push_back(nb);
+}
+
+int		Span::shortestSpan()
+{
+	int	min = INT_MAX;
+
+	if (_v.size() > 1)
+	{		
+		std::vector<int>	a = _v;
+		std::sort(a.begin(), a.end());
+		for (int i = 1; i < a.size(); i++)
+		{
+			if (a[i] - a[i - 1] < min)
+				min = a[i] - a[i - 1];
+		}
+	}
+	else 
+		throw std::length_error("Vector does not have two elements");
+	return min;
+}
+
+int		Span::longestSpan()
+{
+	if (_v.size() < 2)
+		throw std::length_error("Vector does not contain two elements");
+	std::vector<int>::iterator	minit = std::min_element(_v.begin(), _v.end());
+	std::vector<int>::iterator	maxit = std::max_element(_v.begin(), _v.end());
+	return *maxit - *minit;
+}
---
+++ ex01/Span.hpp
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Span.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: spitul <spitul@student.42berlin.de>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/24 12:09:57 by spitul            #+#    #+#             */
+/*   Updated: 2025/08/25 10:34:06 by spitul           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SPAN_HPP
+#define SPAN_HPP
+
+#include <vector>
+
+class Span
+{
+	private:
+		std::vector<int>	_v;
+		int	_N;
+		
+	public:
+		Span(unsigned int N);
+		Span(const Span &other);
+		Span	&operator=(const Span &other);
+		~Span();
+
+		void	addNumber(int nb);
+
+		int		shortestSpan();
+		int		longestSpan();
+};
+
+#endif
